8f29e7488319e71be791e415c2e055d969271a42
mrcamjo12 <130695878+mrcamjo12@users.noreply.github.com>
2023-07-08 17:48:17 -0700

    smart-brain-api

---
+++ controllers/image.js
@@ -0,0 +1,36 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '54fff9c64e9a4d18adcc49bd1f43269d'
+   });
+
+   const handleApiCall = (req, res) => {
+    app.models
+    .predict(
+     {
+       id: 'face-detection',
+       name: 'face-detection',
+       version: '6dc7e46bc9124c5c8824be4822abe105',
+       type: 'visual-detector',
+     }, req.body.input)
+     .then(data => {
+        res.json(data);
+     })
+     .catch(err => res.status(400).json('unable to work with API'))
+   }
+ 
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
